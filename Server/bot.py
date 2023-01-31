2f9621f05864997519e9999eff15612857bca881
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-01-30 20:22:58 -0600

    bot.pt

--- Server/bot.py
+++ Server/bot.py
@@ -3,11 +3,29 @@ import openai_key
 
 openai.api_key = openai_key.openai_key
 
-while True:
-    message = input("Please type anything to initiate a chat with the chat bot:")
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt= message,
-        max_tokens=100
-    )
-    print(response["choices"][0]["text"])
+
+def chat_bot():
+    print("Welcome to the chat bot. Type 'exit or 'quit' to end the chat.")
+    while True:
+        message = input("You: ")
+        if message.lower() in ["exit", "quit"]:
+            break
+        if len(message.strip()) == 0:
+            print("Error: Input cannot be empty.")
+            continue
+        response = None
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=message,
+                max_tokens=100,
+                temperature=1,
+            )
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            continue
+        print("Bot: " + response["choices"][0]["text"])
+
+
+if __name__ == "__main__":
+    chat_bot()
