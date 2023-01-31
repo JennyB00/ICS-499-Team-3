5e78a6f6118b3c1efd0ee9f153cb3fc5a13afd7c
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-01-30 19:55:49 -0600

    bot.pt and openai_key.py

--- Server/bot.py
+++ Server/bot.py
@@ -1,11 +1,17 @@
-import os
 import openai
 import openai_key
+
 openai.api_key = openai_key.openai_key
 
-prompt = "Say this is a test"
+# prompt = "Say this is a test"
+# response = openai.Completion.create(model="text-davinci-003", prompt=prompt)
+# print(response["choices"][0]["text"])
 
-response = openai.Completion.create(model="text-davinci-003", prompt=prompt)
-#print(openai.Model.list())
-print(response["choices"][0]["text"])
-
+while True:
+    message = input("Please type anything to initiate a chat with the chat bot:")
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= message,
+        max_tokens=100
+    )
+    print(response["choices"][0]["text"])
