997d1bf5a64a8f0c6fb75c7e675969241e9b43cc
SantiagoUser <123430594+SantiagoUser@users.noreply.github.com>
2023-02-01 14:37:14 -0600

    First message class commit

--- Server/message.py
+++ Server/message.py
@@ -1,2 +1,18 @@
+import time
 class Message:
-    
+    uid = int()
+    username = str()
+    message = bytearray()
+    messageType = str()
+    date = time.time()
+
+    def createMessage(self, messageType, message):
+        self.messageType = messageType
+        self.message = message
+        return message
+
+    def getMessage():
+        return message
+
+    def getUser():
+        return username
