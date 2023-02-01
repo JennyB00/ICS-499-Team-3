efec44494ccd309e2818e2bcc22c5d28ee61535d
SantiagoUser <123430594+SantiagoUser@users.noreply.github.com>
2023-02-01 14:38:26 -0600

    First contacts class commit

---
+++ Server/contacts.py
@@ -0,0 +1,17 @@
+class Contacts:
+    users = list()
+
+    def add(self, User):
+        self.users.append(User)
+
+    def remove(self, User):
+        self.users.remove(User)
+
+    def getUsers():
+        return users
+
+    def invite(self, user):
+        result = False
+        # send invite, if successful set result to True
+
+        return result
