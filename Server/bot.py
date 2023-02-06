2775466b885606911a9f1a7972ef7d0dbfd86bba
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-02-06 09:31:22 -0600

    bot.py

--- Server/bot.py
+++ Server/bot.py
@@ -1,31 +1,47 @@
 import openai
 import openai_key
 
-openai.api_key = openai_key.openai_key
+
+class Bot:
+    openai.api_key = openai_key.openai_key
+    username = str()
+
+    def generate_image(self):
+        prompt = input("Please describe your image: ")
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="1024x1024"
+        )
+        if response["data"]:
+            image_url = response['data'][0]['url']
+            print("URL: " + image_url)
+        else:
+            print("Error: API request failed. Please try again.")
+
+    def chat_bot(self, username):
+        print("Welcome to the chat bot. Type 'exit or 'quit' to end the chat.")
+        while True:
+            message = input(username + ": ")
+            if message.lower() in ["exit", "quit"]:
+                break
+            if len(message.strip()) == 0:
+                print("Error: Input cannot be empty.")
+                continue
+            response = None
+            try:
+                response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=message,
+                    max_tokens=100,
+                    temperature=1,
+                )
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                continue
+            print("Bot: {}".format(response["choices"][0]["text"].replace(".", ".\n")))
 
 
-def chat_bot():
-    print("Welcome to the chat bot. Type 'exit or 'quit' to end the chat.")
-    while True:
-        message = input("You: ")
-        if message.lower() in ["exit", "quit"]:
-            break
-        if len(message.strip()) == 0:
-            print("Error: Input cannot be empty.")
-            continue
-        response = None
-        try:
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=message,
-                max_tokens=100,
-                temperature=1,
-            )
-        except Exception as e:
-            print(f"Error: {str(e)}")
-            continue
-        print("Bot: {}".format(response["choices"][0]["text"].replace(".", ".\n")))
-
-
-if __name__ == "__main__":
-    chat_bot()
+# if __name__ == "__main__":
+#     bot = Bot()
+#     bot.generate_image()
