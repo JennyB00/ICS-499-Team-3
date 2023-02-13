5aaa2f4fb48d1d955b9196473e5fa0104140ac6d
JennyB00 <LY7168JK@go.minnstate.edu>
2023-02-12 20:32:53 -0600

    UML

--- Server/Domain/bot.py
+++ Server/Domain/bot.py
@@ -5,20 +5,23 @@ import openai_key
 class Bot:
     openai.api_key = openai_key.openai_key
 
-    def __init__(self, username):
+    def __init__(self, username="AI Bot"):
         self.username = username
 
-    def receive(self, messageType, message):
-        return messageType
+    def process(self, prompt: str) -> str:
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                max_tokens=100,
+                temperature=1,
+            )
+            return response["choices"][0]["text"].replace(".", ".\n")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return
 
-    def process(self, message):
-        self.message = message
-
-    def getResponse(self):
-        return self.message
-
-    def generate_image(self):
-        prompt = input("Please describe your image: ")
+    def generate_image(self, prompt) -> str:
         response = openai.Image.create(
             prompt=prompt,
             n=1,
@@ -26,28 +29,29 @@ class Bot:
         )
         if response["data"]:
             image_url = response['data'][0]['url']
-            print("URL: " + image_url)
+            return image_url
         else:
             print("Error: API request failed. Please try again.")
+            return
 
-    def chat_bot(self, username):
-        print("Welcome to the chat bot. Type 'exit or 'quit' to end the chat.")
-        while True:
-            message = input(username + ": ")
-            if message.lower() in ["exit", "quit"]:
-                break
-            if len(message.strip()) == 0:
-                print("Error: Input cannot be empty.")
-                continue
-            response = None
-            try:
-                response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt=message,
-                    max_tokens=100,
-                    temperature=1,
-                )
-            except Exception as e:
-                print(f"Error: {str(e)}")
-                continue
-            print("Bot: {}".format(response["choices"][0]["text"].replace(".", ".\n")))
+    # def chat_bot(self, username):
+    #     print("Welcome to the chat bot. Type 'exit or 'quit' to end the chat.")
+    #     while True:
+    #         message = input(username + ": ")
+    #         if message.lower() in ["exit", "quit"]:
+    #             break
+    #         if len(message.strip()) == 0:
+    #             print("Error: Input cannot be empty.")
+    #             continue
+    #         response = None
+    #         try:
+    #             response = openai.Completion.create(
+    #                 model="text-davinci-003",
+    #                 prompt=message,
+    #                 max_tokens=100,
+    #                 temperature=1,
+    #             )
+    #         except Exception as e:
+    #             print(f"Error: {str(e)}")
+    #             continue
+    #         print("Bot: {}".format(response["choices"][0]["text"].replace(".", ".\n")))
