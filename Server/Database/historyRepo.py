2a5e2348daad10022cf4c64878c18d0b07551d07
JosephCorcoran <123430483+JosephCorcoran@users.noreply.github.com>
2023-02-18 21:56:01 -0600

    Update historyRepo.py

--- Server/Database/historyRepo.py
+++ Server/Database/historyRepo.py
@@ -1,1 +1,13 @@
-# chatUID | accountUID | messageID
+import mysql.connector
+class HistoryRepo:
+    def get_all_history(cursor: mysql.connector.connection.MySQLCursor):
+        query = ("SELECT * FROM history")
+        cursor.execute(query)
+        results = []
+        for (chatID, username, messageDate, messageString, messageFile) in cursor:
+            results.append({"ChatID0":chatID})
+	    results.append({"Username":username})
+	    results.append({"MessageDate":messageDate})
+            results.append({"MessageString":messageString})
+	    results.append({"MessageFile":messageFile})
+        return results
