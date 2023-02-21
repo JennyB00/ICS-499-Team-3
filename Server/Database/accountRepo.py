e53c9a02d1660ab349e3033b58b461c3cc17148e
SantiagoUser <123430594+SantiagoUser@users.noreply.github.com>
2023-02-20 18:40:51 -0600

    Update accountRepo.py

--- Server/Database/accountRepo.py
+++ Server/Database/accountRepo.py
@@ -1,1 +1,11 @@
-# username | password | status | privUID | 
+# username | password | status | privUID |
+import mysql.connector
+class AccountsRepo:
+    def get_all_accounts(cursor: mysql.connector.connection.MySQLCursor):
+        query = ("SELECT * FROM accounts")
+        cursor.execute(query)
+        results = []
+        for (username, password) in cursor:
+            results.append({"Username":username})
+	          results.append({"Password":password})
+        return results
