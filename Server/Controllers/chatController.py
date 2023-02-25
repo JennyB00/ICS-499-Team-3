be48e6642675c2208dca1ae1ac1aa09867035d62
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-02-25 13:51:05 -0600

    delete and update functions

--- Server/Controllers/chatController.py
+++ Server/Controllers/chatController.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter, Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from Domain.chat import *
 from Domain.privileges import Privileges
-from Domain.history import History
+from Domain.History import History
 from Domain.message import *
 from Database.Database import *
-from Database import chatRepo
+from Database.chatRepo import *
 from Database.privilegesRepo import *
 from Database.messageRepo import *
+from Domain.message import *
+
 
 router = APIRouter(
     prefix="/chats",
@@ -16,31 +18,55 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/", response_model=list[Chat])
+
+@router.get("/", response_model=list[ChatPy])
 def read_chats(limit: int = 100, db: Session = Depends(get_db)):
-    return chatRepo.get_all_chats(db, limit=limit)
+    return get_all_chats(db, limit=limit)
 
-@router.get("/{chat_id}", response_model=Chat)
+
+@router.get("/{chat_id}", response_model=ChatPy)
 def read_chat(chat_id: int, db: Session = Depends(get_db)):
-    return chatRepo.get_chat(db, chat_id)
+    return get_chat(db, chat_id)
 
-@router.get("/{chat_id}/privileges", response_model=list[Privileges])
+
+@router.get("/{chat_id}/privileges", response_model=list[PrivilegesPy])
 def read_chat_privileges(chat_id: int, db: Session = Depends(get_db)):
-    return chatRepo.get_privileges_by_chat(db, chat_id)
+    return get_privileges_by_chat(db, chat_id)
 
-@router.get("/{chat_id}/messages", response_model=list[Message])
+
+@router.get("/{chat_id}/messages", response_model=list[MessagePy])
 def read_chat_messages(chat_id: int, db: Session = Depends(get_db)):
-    return chatRepo.get_messages_by_chat(db, chat_id)
+    return get_messages_by_chat(db, chat_id)
 
-@router.post("/", response_model=Chat)
+
+@router.post("/", response_model=ChatPy)
 def create_chat(db: Session = Depends(get_db)):
-    return chatRepo.create_chat(db)
+    return create_chat(db)
 
-@router.post("/{chat_id}/privileges", response_model=Privileges)
+
+@router.post("/{chat_id}/privileges", response_model=PrivilegesPy)
 def create_privileges_for_chat(chat_id: int, privileges: PrivilegesCreate, db: Session = Depends(get_db)):
     db_privilege = create_privileges(db, privileges, chat_id)
-    return db_privilege
+    return PrivilegesPy.from_orm(db_privilege)
 
-@router.post("/{chat_id}/messages", response_model=Message)
+
+@router.post("/{chat_id}/messages", response_model=MessagePy)
 def create_message_for_chat(chat_id: int, message: MessageCreate, db: Session = Depends(get_db)):
     return create_message(db, message, chat_id)
+
+
+@router.delete("/{chat_id}")
+def delete_chat_by_id(chat_id: int, db: Session = Depends(get_db)):
+    if delete_chat(db, chat_id):
+        return {"message": "Success"}
+    else:
+        return {"error": "Error"}
+
+
+@router.put("/{chat_id}", response_model=ChatPy)
+def update_chat_by_id(chat_id: int, chat_update: ChatPy, db: Session = Depends(get_db)):
+    db_chat = get_chat(db, chat_id)
+    if not db_chat:
+        raise HTTPException(status_code=404, detail="Chat not found")
+    updated_chat = update_chat(db, db_chat, chat_update)
+    return updated_chat
