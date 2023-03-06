efbeeca1425311999b37fce7813ce8ac454f7b2f
JennyB00 <LY7168JK@go.minnstate.edu>
2023-03-05 22:22:34 -0600

    account service

--- Server/Service/account_service.py
+++ Server/Service/account_service.py
@@ -3,7 +3,9 @@ from fastapi import Depends
 from Database.database import *
 from Database import account_repo as ar
 
-def login(username: str, password: str, db: Session = Depends(get_db)) -> bool:
+# accounts = ar.get_all_accounts(Depends(get_db),None)
+
+def login_user(username: str, password: str, db: Session = Depends(get_db)) -> bool:
     db_account = ar.get_account(db, username)
     if password is db_account.password:
         db_account.status = "online"
@@ -13,7 +15,7 @@ def login(username: str, password: str, db: Session = Depends(get_db)) -> bool:
     else:
         return False
 
-def logout(username: str, db: Session = Depends(get_db)) -> None:
+def logout_user(username: str, db: Session = Depends(get_db)) -> None:
     db_account = ar.get_account(db, username)
     db_account.status = "offline"
     db.commit()
