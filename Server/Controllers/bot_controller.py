b95c6bf50181116cda6764fb470ccc22a697c72c
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-03-17 22:27:30 -0500

    session_id implemented

--- Server/Controllers/bot_controller.py
+++ Server/Controllers/bot_controller.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import APIRouter
-from Server.Domain.bot import *
+from Server.Domain.bot import Bot
 
 router = APIRouter(
     prefix="/bot",
@@ -8,14 +9,18 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+bot = Bot()
 
-@router.post("/process", response_model=BotBase)
-def process_handler(prompt: str):
-    bot = Bot()
+
+@router.post("/process")
+def process_handler(prompt: str, session_id: Optional[str] = None):
+    if session_id:
+        bot.session_id = session_id
     return bot.process(prompt)
 
 
-@router.post("/generate_image", response_model=BotBase)
-def generate_image_handler(prompt: str):
-    bot = Bot()
+@router.post("/generate_image")
+def generate_image_handler(prompt: str, session_id: Optional[str] = None):
+    if session_id:
+        bot.session_id = session_id
     return bot.generate_image(prompt)
