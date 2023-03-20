d92089bf9be614ff03a36e5949474fcfaf0fd237
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-03-20 15:51:53 -0500

    bot & session_id

--- Server/Controllers/bot_controller.py
+++ Server/Controllers/bot_controller.py
@@ -1,5 +1,7 @@
 from typing import Optional
-from fastapi import APIRouter
+
+import openai
+from fastapi import APIRouter, HTTPException
 from Server.Domain.bot import Bot
 
 router = APIRouter(
@@ -16,11 +18,32 @@ bot = Bot()
 def process_handler(prompt: str, session_id: Optional[str] = None):
     if session_id:
         bot.session_id = session_id
-    return bot.process(prompt)
+    try:
+        response = bot.process(prompt)
+        return {"success": True, "message": response}
+    except openai.error.APIError as e:
+        error_message = f"OpenAI API returned an API Error: {e}"
+        raise HTTPException(status_code=500, detail=error_message)
+    except openai.error.APIConnectionError as e:
+        error_message = f"Failed to connect to OpenAI API: {e}"
+        raise HTTPException(status_code=500, detail=error_message)
+    except openai.error.RateLimitError as e:
+        error_message = f"OpenAI API request exceeded rate limit: {e}"
+        raise HTTPException(status_code=429, detail=error_message)
 
 
 @router.post("/generate_image")
 def generate_image_handler(prompt: str, session_id: Optional[str] = None):
     if session_id:
         bot.session_id = session_id
-    return bot.generate_image(prompt)
+    try:
+        image_url = bot.generate_image(prompt)
+        return {"image_url": image_url}
+    except openai.error.APIError as e:
+        raise HTTPException(status_code=500, detail=f"OpenAI API returned an API Error: {e}")
+    except openai.error.APIConnectionError as e:
+        raise HTTPException(status_code=500, detail=f"Failed to connect to OpenAI API: {e}")
+    except openai.error.RateLimitError as e:
+        raise HTTPException(status_code=429, detail=f"OpenAI API request exceeded rate limit: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error generating image: {e}")
