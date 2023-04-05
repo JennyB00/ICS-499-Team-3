3cf85ae2fe87adaf69027eee0de4e196cd49c3ac
JennyB00 <LY7168JK@go.minnstate.edu>
2023-04-05 12:53:54 -0500

    add landing

--- Frontend/frontend/src/app/app.module.ts
+++ Frontend/frontend/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 import { ChatComponent } from './chat/chat.component';
 import { ProfileComponent } from './profile/profile.component';
-
-import { FormsModule } from '@angular/forms';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
     ChatComponent,
-    ProfileComponent
+    ProfileComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
