fa71b73ae88017f26a24576f96422651991b68d9
Luong Nguyen <78390039+freezeb421@users.noreply.github.com>
2023-04-10 11:58:06 -0500

    BotComponent

--- Frontend/frontend/src/app/app.module.ts
+++ Frontend/frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
@@ -26,7 +27,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/frontend/src/app/bot/bot.component.ts
+++ Frontend/frontend/src/app/bot/bot.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bot',
@@ -11,7 +12,7 @@ export class BotComponent {
   messages: string[] = [];
   newMessage = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   homePage() {
     // navigate to the home page
@@ -19,9 +20,22 @@ export class BotComponent {
   }
 
   submit() {
-    if (this.newMessage.trim() !== '') {
-      this.messages.push(this.newMessage);
-      this.newMessage = '';
-    }
-  }
+  console.log('Input was: ', this.newMessage)
+
+  if (this.newMessage.trim() !== '') {
+  // send the message to the server using POST request
+  this.http.post('http://localhost:8000/bot/process', { prompt: this.newMessage})
+    .subscribe({
+      next: (response) => {
+        console.log('Message sent successfully');
+        console.log('Input was:', this.newMessage);
+        console.log('Response:', response);
+        this.messages.push(this.newMessage);
+        this.newMessage = '';
+      },
+      error: (error) => {
+        console.error('Error sending message: ', error);
+        console.log('Error response body: ', error.error);
+      }
+    });
 }
