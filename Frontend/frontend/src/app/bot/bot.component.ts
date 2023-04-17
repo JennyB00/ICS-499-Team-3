0d78d56fb651c69d809e170f27bb57982c90f2e5
JennyB00 <LY7168JK@go.minnstate.edu>
2023-04-17 17:48:22 -0500

    bot request fix

--- Frontend/frontend/src/app/bot/bot.component.ts
+++ Frontend/frontend/src/app/bot/bot.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { UserService } from '../user.service';
 
 @Component({
@@ -26,13 +26,14 @@ export class BotComponent implements OnInit{
 
     if (this.newMessage.trim() !== '') {
       // send the message to the server using POST request
-      this.http.post('http://localhost:8000/bot/process', { prompt: this.newMessage})
+      this.http.post('http://localhost:8000/bot/process','', { params: new HttpParams().set('prompt',this.newMessage).set('username',this.userService.getCurrentUser())})
         .subscribe({
           next: (response) => {
             console.log('Message sent successfully');
             console.log('Input was:', this.newMessage);
             console.log('Response:', response);
-            this.messages.push(this.newMessage);
+            this.messages.push(this.userService.getCurrentUser()+' - '+this.newMessage);
+            this.messages.push('Bot - '+response);
             this.newMessage = '';
           },
           error: (error) => {
