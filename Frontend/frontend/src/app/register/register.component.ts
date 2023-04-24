dc3c08ed80ab90f76c093379fe663ad523b3d9b0
Joseph <joe.corcoran2014@gmail.com>
2023-04-24 16:05:12 -0500

    Added Validators and connection to backend

--- Frontend/frontend/src/app/register/register.component.ts
+++ Frontend/frontend/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserCreate, UserService } from '../user.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +8,42 @@ import { Router } from '@angular/router';
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
     registrationComplete: boolean = false;
+    registrationForm: FormGroup;
+    usernameTaken: boolean = false;
 
-    constructor(private router: Router) {}
+    constructor(private userService: UserService, private router: Router) {}
+
+    ngOnInit(){
+        this.registrationForm = new FormGroup({
+            username: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+              ])),
+            password: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+              ]))
+        });
+    }
+
+    onSubmit(value: any){
+        const username: string = value.username;
+        const password: string = value.password;
+        const usernames = this.userService.getUsernames();
+        for(var index in usernames){
+            if(username == usernames[index]){
+                this.usernameTaken = true;
+                break;
+            }
+        }
+        if(this.usernameTaken == false){
+            const newUser: UserCreate = {username: username, password: password, status: 'offline'};
+            this.userService.add(newUser);
+            this.registrationComplete = true;
+        }
+    }
 
     onRegistrationComplete() {
         this.registrationComplete = true;
